bd08f64a959ecd2210146b62a35c897ef061dfca
Danni <danielk21@ru.is>
2022-05-04 17:14:29 +0000

    catalogue site added, incomplete

--- fire_sale/catalogue/views.py
+++ fire_sale/catalogue/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from catalogue.models import Postings
 
 # Create your views here.
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return render(request, 'catalougue/index.html')
+    context = {'postings': Postings.objects.all()}
+    return render(request, 'catalogue/index.html', context)
