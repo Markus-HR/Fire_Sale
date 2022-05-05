d4d10941614e1fc640d6d9b0a441b048cbb90d08
Markus-HR <markusf21@ru.is>
2022-05-05 16:14:53 +0000

    added style

--- fire_sale/static/python/CustomUserForms.py
+++ fire_sale/static/python/CustomUserForms.py
@@ -9,38 +9,56 @@ class RegisterForm(UserCreationForm):
         fields = ('email', 'username', 'password1', 'password2')
 
     email = forms.EmailField(
+        label='Email',
         widget=forms.EmailInput(
             attrs={'placeholder': 'Email',
-                   'style': 'width: 300px;',
+                   'style': 'width: 300px;'
+                            'display: block;'
+                            'margin : 0 auto;',
                    'class': 'form-control'}))
 
     username = forms.CharField(
+        label='Username',
         widget=forms.TextInput(
-            attrs={'placeholder': 'Email',
-                   'style': 'width: 300px;',
+            attrs={'placeholder': 'Username',
+                   'style': 'width: 300px;'
+                            'display: block;'
+                            'margin : 0 auto;',
                    'class': 'form-control'}))
 
     password1 = forms.CharField(
+        label='Password',
         widget=forms.PasswordInput(
-            attrs={'placeholder': 'password',
-                   'style': 'width: 300px;',
+            attrs={'placeholder': 'Password',
+                   'style': 'width: 300px;'
+                            'display: block;'
+                            'margin : 0 auto;',
                    'class': 'form-control'}))
     password2 = forms.CharField(
+        label='Confirm Password',
         widget=forms.PasswordInput(
-            attrs={'placeholder': 'password',
-                   'style': 'width: 300px;',
+            attrs={'placeholder': 'Password',
+                   'style': 'width: 300px;'
+                            'display: block;'
+                            'margin : 0 auto;',
                    'class': 'form-control'}))
 
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
+        label='Username',
         widget=forms.TextInput(
             attrs={'placeholder': 'Email',
-                   'style': 'width: 300px;',
+                   'style': 'width: 300px;'
+                            'display: block;'
+                            'margin : 0 auto;',
                    'class': 'form-control'}))
 
     password = forms.CharField(
+        label='Password',
         widget=forms.PasswordInput(
             attrs={'placeholder': 'password',
-                   'style': 'width: 300px;',
+                   'style': 'width: 300px;'
+                            'display: block;'
+                            'margin : 0 auto;',
                    'class': 'form-control'}))
