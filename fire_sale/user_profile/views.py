60b235d5cf6c34066b1890deb6053d243e8a87c7
Markus-HR <markusf21@ru.is>
2022-05-05 12:57:12 +0000

    added user register & login

--- fire_sale/user_profile/views.py
+++ fire_sale/user_profile/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from static.python.CustomUserCreationForm import RegisterForm
+from static.python.UserForms import RegisterForm, UserLogin
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'user/register.html', {
+        'form': form
+    })
+
+
+def login(request):
+    form = UserLogin()
+    if form.is_valid():
+        form.save()
+        return redirect('login')
+
+    return render(request, 'user/login.html', {
+        'form': form
+    })
